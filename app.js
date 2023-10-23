722c991e5f3403f53661c33b308419ee4e9f1191
Umer Sajjad <umersajjad8@gmail.com>
2023-10-23 19:47:26 +0500

    abc

---
+++ app.js
@@ -0,0 +1,58 @@
+
+
+//Assignment 1
+
+alert('Error! Please enter valid Password');
+
+alert('Welcome to JS Land \nHappy Coding!');
+
+alert('Welcome to JS Land');
+
+alert('Happy Coding!');
+
+alert('Hello... I can run JS through my web brower console');
+
+console.log('Hello... I can run JS through my web brower console');
+
+
+
+// Assignment 2
+
+let username = prompt('Enter Your Name');
+alert('Your name is ' + username);
+
+let userage = prompt('Enter Your Age');
+alert('Your age is ' + userage);
+
+let useruni = prompt('Enter Your Uni Name');
+alert('Your Uni Name is ' + useruni);
+
+let usercourse = prompt('Enter Your Course Name');
+alert('Your Course name is ' + usercourse);
+
+alert('Pizza\nPizz\nPiz\nPi\nP')
+
+let city = prompt('Enter Your City Name');
+alert('Your name is ' + username + ' Your age is ' + userage + ' Your Uni Name is ' + useruni + ' Your Course name is ' + usercourse + ' You lived in ' + city);
+
+let email = prompt('Enter Your Email');
+alert('Your Email is ' + email);
+
+alert('I am trying to learn from a book Name A smarter way to learn JS')
+
+console.log('Your name is ' + username + ' Your age is ' + userage + ' Your Uni Name is ' + useruni + ' Your Course name is ' + usercourse + ' You lived in ' + city);
+
+console.log('Yay I Can write HTML Content Through JS')
+
+
+
+
+
+
+
+
+
+
+
+
+
